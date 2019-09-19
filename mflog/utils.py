1e469ac6dcc59c80ecb0635da81a1df5b1c0a548
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-09-19 10:12:48 +0200

    feat:  feat: replace MODULE* environment variables names by MFMODULE* (MODULE_HOME becomes MFMODULE_HOME and so on)

--- mflog/utils.py
+++ mflog/utils.py
@@ -13,10 +13,12 @@ LEVEL_FROM_LOGGER_NAME_CACHE = {}
 
 # metwork stuff
 MFCOM_HOME = os.environ.get('MFCOM_HOME', None)
-MODULE_HOME = os.environ.get('MFMODULE_HOME', os.environ.get('MODULE_HOME', None))
-MODULE_RUNTIME_HOME = os.environ.get('MFMODULE_RUNTIME_HOME', 
-                                     os.environ.get('MODULE_RUNTIME_HOME', None))
-MODULE = os.environ.get('MFMODULE', os.environ.get('MODULE', 'UNKNOWN'))
+MFMODULE_HOME = os.environ.get('MFMODULE_HOME',
+                               os.environ.get('MODULE_HOME', None))
+MFMODULE_RUNTIME_HOME = os.environ.get('MFMODULE_RUNTIME_HOME',
+                                       os.environ.get('MODULE_RUNTIME_HOME',
+                                                      None))
+MFMODULE = os.environ.get('MFMODULE', os.environ.get('MODULE', 'UNKNOWN'))
 
 
 def write_with_lock(f, message):
@@ -89,7 +91,7 @@ class Config(object):
             if self._minimal_level is None:
                 # metwork mode
                 self._minimal_level = \
-                    os.environ.get('%s_LOG_MINIMAL_LEVEL' % MODULE, 'INFO')
+                    os.environ.get('%s_LOG_MINIMAL_LEVEL' % MFMODULE, 'INFO')
         if json_minimal_level is not None:
             self._json_minimal_level = json_minimal_level
         else:
@@ -98,7 +100,7 @@ class Config(object):
             if self._json_minimal_level is None:
                 # metwork mode
                 self._json_minimal_level = \
-                    os.environ.get('%s_LOG_JSON_MINIMAL_LEVEL' % MODULE,
+                    os.environ.get('%s_LOG_JSON_MINIMAL_LEVEL' % MFMODULE,
                                    'WARNING')
         if json_file is not None:
             self._json_file = json_file
@@ -106,7 +108,7 @@ class Config(object):
             self._json_file = os.environ.get("MFLOG_JSON_FILE", None)
             if self._json_file is None:
                 # metwork mode
-                self._json_file = os.environ.get('%s_LOG_JSON_FILE' % MODULE,
+                self._json_file = os.environ.get('%s_LOG_JSON_FILE' % MFMODULE,
                                                  None)
             if self._json_file == "null":
                 self._json_file = None
@@ -120,7 +122,7 @@ class Config(object):
             else:
                 # metwork mode
                 self._override_files = []
-                for env in MODULE_RUNTIME_HOME, MODULE_HOME, MFCOM_HOME:
+                for env in MFMODULE_RUNTIME_HOME, MFMODULE_HOME, MFCOM_HOME:
                     if env:
                         self._override_files.append(
                             "%s/config/mflog_override.conf" % env
